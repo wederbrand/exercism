dcbc8e9a0fd8680d280c1b051da8bd37df67dce4
Andreas Wederbrand <andreas.wederbrand@verisure.com>
2020-09-30 17:48:02 +0200

    final solution for robot name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -4,60 +4,42 @@ package robotname
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-var allNames = make([]int, 0)
-var nameIndex = 0
+var allNames = make(map[string]bool)
 
-// Generate all possible names once then just randomly put them in a list of names.
-// Keep track of which names has been used by moving an index forward when names are used.
-// Returns new names at constant price as opposed to generate a new random name until one
-// that hasn't been seen before is found, which would require generating on average
-// 338.000 random names until the last one is found.
-func init() {
-	tempMap := make(map[int]struct{})
-	for i := 0; i < 26*26*1000; i++ {
-		tempMap[i] = struct{}{}
-	}
-
-	for k := range tempMap {
-		allNames = append(allNames, k)
-	}
-}
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // Robot represents a robot with a name and very little else.
 type Robot struct {
 	name string
 }
 
-func (r *Robot) generateName() error {
-	if nameIndex == 26*26*1000 {
-		return errors.New("all names used")
-	}
-
-	r.name = convertToName(allNames[nameIndex])
-	nameIndex++
-	// fmt.Printf("%d\n", nameIndex)
-	return nil
-}
-
-func convertToName(i int) string {
-	a := i % 26
-	i /= 26
-	b := i % 26
-	i /= 26
-
-	return fmt.Sprintf("%c%c%03d", a+'A', b+'A', i)
+func randomName() string {
+	r1 := random.Intn(26) + 'A'
+	r2 := random.Intn(26) + 'A'
+	num := random.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
 
 // Name returns the name of the robot.
 func (r *Robot) Name() (string, error) {
-	if r.name == "" {
-		err := r.generateName()
-		if err != nil {
-			return "", err
-		}
+	if r.name != "" {
+		return r.name, nil
 	}
+
+	if len(allNames) == 26*26*1000 {
+		return "", errors.New("all names used")
+	}
+
+	r.name = randomName()
+	for allNames[r.name] {
+		r.name = randomName()
+	}
+	allNames[r.name] = true
+
 	return r.name, nil
 }
 
